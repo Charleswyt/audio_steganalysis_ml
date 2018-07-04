83e561e95f5eed7f91e1595f5eae58a19c616c01
Charles_wyt <wangyuntao16@mails.ucas.ac.cn>
2018-07-04 10:16:15 +0800

    update traning function

--- train_test/training.m
+++ train_test/training.m
@@ -1,8 +1,8 @@
 %% training
-% - [result, model, predict_label, ground_truth] = ...
-%       training(cover_feature, stego_feature, percent, n, model_file_name, seek_best_params, svm_params, is_rewrite)
+% - [result, model, predict, ground_truth] = ...
+%      training(cover_feature, stego_feature, percent, n, model_file_name, seek_best_params, svm_params, is_rewrite)
 % - Variable:
-% ------------------------------------------input
+%------------------------------------------input
 % cover_feature         the feature of cover samples
 % stego_feature         the feature of stego samples
 % percent               the percent of training set, default is 0.8
@@ -11,19 +11,19 @@
 % svm_params            svm parameters for training
 % seek_best_paarams     whether seek the optimal parameters for training
 % is_rewrite            whether to rewrite the model file
-% -----------------------------------------output
+%-----------------------------------------output
 % result                result
-%    FPR                False positive rate
-%    FNR                False negative rate
-%    ACC                Accuracy
+%   FPR                 False positive rate
+%   FNR                 False negative rate
+%   ACC                 Accuracy
 % model                 model
-% predict_label         predictal label
+% predict               predictal label
 % ground_truth          real label
 
-function [result, model, predict_label, ground_truth] ...
+function [result, prob, model, predict, ground_truth] ...
     = training(cover_feature, stego_feature, percent, n, model_file_name, seek_best_params, svm_params, is_rewrite)
 
-% default parameters
+%default parameters
 if ~exist('percent', 'var') || isempty(percent)
     percent = 0.8;
 end
@@ -46,7 +46,7 @@ end
 
 if ~exist('svm_params', 'var') || isempty(svm_params)
     if strcmp(seek_best_params, 'False')
-        svm_params = '-s 0 -t 0 -c 1024 -g 0.03125'; % 1024
+        svm_params = '-s 0 -t 0 -c 256 -g 1024'; %1024
     elseif strcmp(seek_best_params, 'True')
         [best_acc, best_t, bestc, bestg] = get_best_params(cover_feature, stego_feature, percent);
         svm_params = ['-s 0 -t ', num2str(best_t), '-c ', num2str(bestc), ' -g ', num2str(bestg)];
@@ -63,63 +63,40 @@ stego_label =  ones(sample_num_stego, 1);                                   % st
 cover_data = [cover_feature, cover_label];                                  % cover data [feature, label]
 stego_data = [stego_feature, stego_label];                                  % stego data [feature, label]
 data = [cover_data; stego_data];                                            % data and label pair
-
-FPR = zeros(1, n);                                                          % the initialization of FPR
-FNR = zeros(1 ,n);                                                          % the initialization of FNR
-ACC = zeros(1 ,n);                                                          % the initialization of ACC
-
-predict_label = [];                                                         % the initialization of test label
-ground_truth  = [];                                                         % the initialization of real label
+data = shuffle(data);                                                       % data shuffle
 
 start_time = tic;
 
-for i = 1 : n
-    
-    temp = shuffle(data);                                                   % the shuffle of training data
+%% make data
+train_data  = data(1:train_set_number, 1:feature_dimension);                % training data
+train_label = data(1:train_set_number, feature_dimension+1);                % training label
+test_data   = data(train_set_number+1:end, 1:feature_dimension);            % test data
+test_label  = data(train_set_number+1:end, feature_dimension+1);            % test label
 
-    %% make data
-    train_data  = temp(1:train_set_number, 1:feature_dimension);            % training data
-    train_label = temp(1:train_set_number, feature_dimension+1);            % training label
-    test_data   = temp(train_set_number+1:end, 1:feature_dimension);        % test data
-    test_label  = temp(train_set_number+1:end, feature_dimension+1);        % test label
-    
-    %% SVM training
-    svm_model(i)    = svmtrain(train_label, train_data, svm_params);        %#ok<SVMTRAIN,AGROW>
-    
-    %% SVM validation
-    predict = svmpredict(test_label, test_data, svm_model(i));
-    predict_label = [predict_label; predict];                               %#ok<AGROW>
-    ground_truth  = [ground_truth; test_label];                             %#ok<AGROW>
-    
-    FP = sum(test_label == -1 & predict ==  1);                             % False Positive
-    FN = sum(test_label ==  1 & predict == -1);                             % False Negative
-    TP = sum(test_label ==  1 & predict ==  1);                             % True  Positive
-    TN = sum(test_label == -1 & predict == -1);                             % True  Positive
-        
-    FPR(i) = FP / (FP + TN);                                                % False Positive Rate
-    FNR(i) = FN / (TP + FN);                                                % False Negative Rate
-    ACC(i) = 1 - ((FPR(i) + FNR(i)) / 2);                                   % Accuracy
-    
-    model(i).svm_model = svm_model(i);                                      %#ok<AGROW>
-    model(i).FPR = FPR(i);                                                  %#ok<AGROW>
-    model(i).FNR = FNR(i);                                                  %#ok<AGROW>
-    model(i).ACC = ACC(i);                                                  %#ok<AGROW>
-    
-    %% save the model file
-    if strcmp(is_rewrite, 'True') == 1
-        if ~exist(model_file_name, 'file')
-            save(model_file_name, 'model', '-append');
-        else
-            save(model_file_name, 'model');
-        end
-    else
-        continue;
-    end
+%% SVM training
+model = libsvmtrain(train_label, train_data, svm_params);
+
+%% SVM validation
+[predict, ~, prob] = libsvmpredict(test_label, test_data, model, '-b 1');
+ground_truth  = test_label;
+
+FP = sum(test_label == -1 & predict ==  1);                                 % False Positive
+FN = sum(test_label ==  1 & predict == -1);                                 % False Negative
+TP = sum(test_label ==  1 & predict ==  1);                                 % True  Positive
+TN = sum(test_label == -1 & predict == -1);                                 % True  Positive
+
+FPR = FP / (FP + TN);                                                       % False Positive Rate
+FNR = FN / (TP + FN);                                                       % False Negative Rate
+ACC = 1 - ((FPR + FNR) / 2);                                                % Accuracy
+
+%% save the model file
+if strcmp(is_rewrite, 'True') == 1
+    save(model_file_name, 'model');
 end
 
-result.FPR = mean(FPR);
-result.FNR = mean(FNR);
-result.ACC = mean(ACC);
+result.FPR = FPR;
+result.FNR = FNR;
+result.ACC = ACC;
 result.svm_params = svm_params;
 
 if strcmp(seek_best_params, 'True')
