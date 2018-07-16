5e5f089f25ea8c36bc9b08dfa4d14c0901af40a8
Charles_wyt <wangyuntao16@mails.ucas.ac.cn>
2018-07-17 00:13:50 +0800

    batch process

--- feature_extract/feature_analysis/preprocessing_select.m
+++ feature_extract/feature_analysis/preprocessing_select.m
@@ -5,34 +5,40 @@ T = 3;
 [max_mean_value, max_var, max_skew, max_kurt] = deal(0,0,0,0);
 preprocessing_methods = {'origin', 'dif1_h', 'dif1_v', 'dif2_h', 'dif2_v', 'abs_dif1_h', 'abs_dif1_v', 'abs_dif2_h', 'abs_dif2_v'};
 
-cover = load('E:\Myself\2.database\mtap\txt\cover\128\wav10s_00005.txt');
-stego = load('E:\Myself\2.database\mtap\txt\stego\EECS\EECS_W_2_B_128_ER_05\wav10s_00005_stego_128.txt');
+file_num = 1;
+
+data_cover_path = 'E:\Myself\2.database\mtap\txt\cover\128';
+data_stego_path = 'E:\Myself\2.database\mtap\txt\stego\EECS\EECS_W_2_B_128_ER_05';
+
+[cover_files_list, cover_file_num] = get_files_list(data_cover_path, 'txt');
+[stego_files_list, stego_file_num] = get_files_list(data_stego_path, 'txt');
 
 for i = 1:length(preprocessing_methods)
     preprocessing_method = preprocessing_methods{i};
+    [index1, index2, index3, index4] = deal(0,0,0,0);
     
-    [cover_mean_value, cover_var, cover_skew, cover_kurt] = preprocessing(cover, preprocessing_method, T);
-    [stego_mean_value, stego_var, stego_skew, stego_kurt] = preprocessing(stego, preprocessing_method, T);
-    
-    dif_mean_value = cover_mean_value - stego_mean_value;
-    dif_var = cover_var - stego_var;
-    dif_skew = cover_skew - stego_skew;
-    dif_kurt = cover_kurt - stego_kurt;
-    
-    distance = get_distance(cover, stego, preprocessing_method, T, 'euclidean');
-    
+    for f = 1:file_num
+        cover_file_path = fullfile(data_cover_path, cover_files_list{f});
+        stego_file_path = fullfile(data_stego_path, stego_files_list{f});
+        
+        cover = load(cover_file_path);
+        stego = load(stego_file_path);
+        distance = get_distance(cover, stego, preprocessing_method, T);
+        index1  = index1 + distance.euclidean_distance;
+        index2  = index2 + distance.cosine;
+        index3  = index3 + distance.pearson_correlation;
+        index4  = index4 + distance.KL;
+    end
     fprintf('=============================\n');
-    fprintf('Euclidean Distance : %.3f\n', distance.euclidean_distance);
-    fprintf('Cosine Similarity  : %.3f\n', distance.cosine);
-    fprintf('Pearson Correlation: %.3f\n', distance.pearson_correlation);
+    fprintf('%s:\n', preprocessing_method);
+    fprintf('Euclidean Distance : %.3f\n', index1/file_num);
+    fprintf('Cosine Similarity  : %.3f\n', index2/file_num);
+    fprintf('Pearson Correlation: %.3f\n', index3/file_num);
+    fprintf('Kullback¨CLeibler divergence: %.3f\n', index4/file_num);
 end
 
-fprintf('max mean    : %.5f\n', max_mean_value);
-fprintf('max variance: %.5f\n', max_var);
-fprintf('max skewness: %.5f\n', max_skew);
-fprintf('max kurtosis: %.5f\n', max_kurt);
-    
-% [mean_value, var, skew, kurt] = preprocessing_select(preprocessing_method)
+
+% [mean_value, var, skew, kurt] = preprocessing(matrix, preprocessing_method, T)
 % - Variable:
 % ------------------------------------------input
 % matrix                    QMDCT coefficiens matrix
@@ -44,7 +50,7 @@ fprintf('max kurtosis: %.5f\n', max_kurt);
 % skew                      skewness of feature vector
 % kurt                      kurtosis of feature vector
 
-function [mean_value, var, skew, kurt] = preprocessing(matrix, preprocessing_method, T)
+function [mean_value, var, skew, kurt] = preprocessing(matrix, preprocessing_method, T) %#ok<*DEFNU>
 
 if strcmp(preprocessing_method, 'origin') new_matrix = matrix;end
 
@@ -64,23 +70,19 @@ feature = get_point_block_markov(new_matrix, T);
 
 end
 
-
-% function distance = get_distance(vector1, vector2, metric)
+% distance = get_distance(matrix_cover, matrix_stego, preprocessing_method, T)
 % - Variable:
 % ------------------------------------------input
 % matrix_cover              cover QMDCT coefficients matrix
 % matrix_stego              stego QMDCT coefficients matrix
-% metric                    metric
 % -----------------------------------------output
 % distance
-%   euclidean               euclidean distance
+%   euclidean_distance       euclidean distance
+%   cosine                   cosine similarity
+%   pearson_correlation      Pearson correlation
+%   KL                       Kullback¨CLeibler divergence
 
-% Note:
-%   methods = {'euclidean'; 'seuclidean'; 'cityblock'; 'chebychev'; ...
-%            'mahalanobis'; 'minkowski'; 'cosine'; 'correlation'; ...
-%            'spearman'; 'hamming'; 'jaccard';'squaredEuclidean'};
-
-function distance = get_distance(matrix_cover, matrix_stego, preprocessing_method, T, metric)
+function distance = get_distance(matrix_cover, matrix_stego, preprocessing_method, T)
 
 if strcmp(preprocessing_method, 'origin') new_matrix_cover = matrix_cover;new_matrix_stego = matrix_stego;end
 
@@ -94,18 +96,26 @@ if strcmp(preprocessing_method, 'abs_dif1_v') new_matrix_cover = pre_process_mat
 if strcmp(preprocessing_method, 'abs_dif2_h') new_matrix_cover = pre_process_matrix(matrix_cover, 'abs_dif2_h');new_matrix_stego = pre_process_matrix(matrix_stego, 'abs_dif1_h');end
 if strcmp(preprocessing_method, 'abs_dif2_v') new_matrix_cover = pre_process_matrix(matrix_cover, 'abs_dif2_v');new_matrix_stego = pre_process_matrix(matrix_stego, 'abs_dif1_h');end
 
+% feature extraction
 feature_cover = get_point_block_markov(new_matrix_cover, T);
 feature_stego = get_point_block_markov(new_matrix_stego, T);
 
+% euclidean distance
 distance.euclidean_distance = sqrt(sum((feature_cover - feature_stego).^2));
+
+% cosine similarity
 numerator = sum(feature_cover.*feature_stego);
 denominator = sqrt(sum(feature_cover.^2)) * sqrt(sum(feature_stego.^2));
 distance.cosine = numerator / denominator;
 
+% Pearson correlation
 feature_cover_new = feature_cover - mean(feature_cover);
 feature_stego_new = feature_stego - mean(feature_stego);
 numerator = sum(feature_cover_new.*feature_stego_new);
 denominator = sqrt(sum(feature_cover_new.^2)) * sqrt(sum(feature_stego_new.^2));
 distance.pearson_correlation = numerator / denominator;
+
+H = sum(feature_cover.*log(feature_cover./feature_stego));
+distance.KL = H / 6 * (2*T+1);
 
 end
