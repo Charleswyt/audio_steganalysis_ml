38483798a6946fc6cace31f22978783bc6e07391
Charles_wyt <wangyuntao16@mails.ucas.ac.cn>
2018-07-21 00:03:54 +0800

    update

--- feature_extract/base_modules/get_point_block_markov.m
+++ feature_extract/base_modules/get_point_block_markov.m
@@ -8,10 +8,15 @@
 
 function features = get_point_block_markov(matrix, T)
 
-feature1 = get_markov(matrix, 'hv', T, 1);                                  % point-wise correlation
-feature2 = get_block_markov(matrix, 'hv', 2, T, 1);                         % 2 x 2 block-wise correlation
-feature3 = get_block_markov(matrix, 'hv', 4, T, 1);                         % 4 x 4 block-wise correlation
+feature1 = get_markov(matrix, 'h', T, 1);                                  % point-wise correlation
+feature2 = get_markov(matrix, 'v', T, 1);                                  % point-wise correlation
 
-features = [feature1;feature2;feature3];
+feature3 = get_block_markov(matrix, 'h', 2, T, 1);                         % 2 x 2 block-wise correlation
+feature4 = get_block_markov(matrix, 'v', 2, T, 1);                         % 2 x 2 block-wise correlation
+
+feature5 = get_block_markov(matrix, 'h', 4, T, 1);                         % 4 x 4 block-wise correlation
+feature6 = get_block_markov(matrix, 'v', 4, T, 1);                         % 4 x 4 block-wise correlation
+
+features = [feature1;feature2;feature3;feature4;feature5;feature6];
 
 end
