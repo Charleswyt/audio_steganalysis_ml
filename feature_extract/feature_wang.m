f1a79a1dabe90eeeb382fd2f2a044f6e96154f28
Charles_wyt <wangyuntao16@mails.ucas.ac.cn>
2018-07-22 09:14:15 +0800

    update

--- feature_extract/feature_wang.m
+++ feature_extract/feature_wang.m
@@ -9,12 +9,28 @@
 
 function feature = feature_wang(matrix, T)
 
-matrix_dif2_v = pre_process_matrix(matrix, 'dif2_v');
-matrix_abs_dif1_v = pre_process_matrix(matrix, 'abs_dif1_v');
+matrix_dif1_v = pre_process_matrix(matrix, 'dif1_v');
+% matrix_dif1_h = pre_process_matrix(matrix, 'dif1_h');
+
+% matrix_dif2_v = pre_process_matrix(matrix, 'dif2_v');
+% matrix_dif2_h = pre_process_matrix(matrix, 'dif2_h');
+
+% matrix_abs_dif1_v = pre_process_matrix(matrix, 'abs_dif1_v');
+matrix_abs_dif1_h = pre_process_matrix(matrix, 'abs_dif1_h');
+
 matrix_abs_dif2_v = pre_process_matrix(matrix, 'abs_dif2_v');
+matrix_abs_dif2_h = pre_process_matrix(matrix, 'abs_dif2_h');
 
 feature1 = get_point_block_markov(matrix, T);
-feature2 = get_point_block_markov(matrix_dif2_v, T);
-feature3 = get_point_block_markov(matrix_abs_dif2_v, T);
 
-feature = [feature1;feature2;feature3];
+feature2 = get_point_block_markov(matrix_dif1_v, T);
+feature3 = [];%get_point_block_markov(matrix_dif1_h, T);
+feature4 = [];%get_point_block_markov(matrix_dif2_v, T);
+feature5 = [];%= get_point_block_markov(matrix_dif2_h, T);
+
+feature6 = [];%get_point_block_markov(matrix_abs_dif1_v, T);
+feature7 = get_point_block_markov(matrix_abs_dif1_h, T);
+feature8 = get_point_block_markov(matrix_abs_dif2_v, T);
+feature9 = get_point_block_markov(matrix_abs_dif2_h, T);
+
+feature = [feature1;feature2;feature3;feature4;feature5;feature6;feature7;feature8;feature9];
