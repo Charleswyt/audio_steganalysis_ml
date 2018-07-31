2e39d9e515f04fc013ff7c22856bffd41f1f5178
Charles_wyt <wangyuntao16@mails.ucas.ac.cn>
2018-07-31 20:19:44 +0800

    checkerboard plot

---
+++ feature_extract/curve_plot/checkerboard_plot.m
@@ -0,0 +1,33 @@
+%% feature selection
+% - checkerboard_plot(matrix, side)
+% - Variables:
+% ------------------------------------------input
+% matrix            input data
+% side              side length of checkboard
+% -----------------------------------------output
+% Null
+
+function checkerboard_plot(matrix, side)
+
+if ~exist('side', 'var') || isempty(side)
+    shape = size(matrix);
+    side = shape(1);
+end
+
+imagesc(matrix);                                                            % Create a colored plot of the matrix values
+colormap(flipud(gray));                                                     % Change the colormap to gray (so higher values are
+
+T = (side - 1) / 2;
+
+for t = -T:T
+    scale{t+T+1} = num2str(t);                                              %#ok<AGROW>
+end
+
+set(gca, ...
+    'XTick',1:side,...                                                      % Change the axes tick marks
+    'XTickLabel',scale,...                                                  % and tick labels
+    'YTick',1:side,...
+    'YTickLabel',scale,...
+    'TickLength',[0 0]);
+xlabel('Q_{ij}', 'FontSize', 12);
+ylabel('Q_{ij}', 'FontSize', 12);
