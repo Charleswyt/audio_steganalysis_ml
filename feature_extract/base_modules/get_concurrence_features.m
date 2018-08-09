bc36151cbe76bd14ff08de96906ac4696db29be8
Charles_wyt <Charleswyt@users.noreply.github.com>
2018-08-09 13:03:25 +0800

    update

--- feature_extract/base_modules/get_concurrence_features.m
+++ feature_extract/base_modules/get_concurrence_features.m
@@ -11,7 +11,7 @@ function features = get_concurrence_features(matrix, T)
 
 % default parameters
 if ~exist('matrix', 'var') || isempty(matrix)
-    matrix = floor(10 * (rand(10) - 0.5));
+    matrix = floor(10 * rand(10));
 end
 
 if ~exist('T', 'var') || isempty(T)
@@ -22,9 +22,9 @@ end
 Ad = max(min(matrix,T),-T);
 
 % statistical of concurrence matrix
-[height, width] = size(Ad);
-mean_concurrence = mean(Ad(:));
-concurrence_x = sum(Ad, 2);                                               % P_{i,0,x}
+slide = size(Ad, 1);                                                                % slide of concurence matrix
+mean_concurrence = mean(Ad(:));                                      % mean of concurence matrix
+concurrence_x = sum(Ad, 2)';                                              % P_{i,0,x}
 concurrence_y = sum(Ad, 1);                                               % P_{i,0,y}
 
 % feature extraction
@@ -33,27 +33,24 @@ features(1) = sum(Ad(:).^2);
 features(2) = max(Ad(:));
 features(3) = -sum(Ad(:) .* log(Ad(:) + eps));
 
-for h = 1:height
-    for w = 1:width
+for h = 1:slide
+    for w = 1:slide
         features(4) = features(4) + Ad(h, w) / (1 + (h - w)^2);
         features(5) = features(5) + (h - mean_concurrence)^2 * Ad(h, w);
         features(6) = features(6) - Ad(h, w) * log(concurrence_x(h) * concurrence_y(w)+eps);
         features(7) = features(7) - concurrence_x(h) * concurrence_y(w) * log(concurrence_x(h) * concurrence_y(w)+eps);
     end
     features(8) = features(8) - Ad(h, h) * log(Ad(h, h) + eps);
-    features(9) = features(9) - Ad(h, height - h + 1) * log(Ad(h, height - h + 1)+eps);
+    features(9) = features(9) - Ad(h, slide - h + 1) * log(Ad(h, slide - h + 1)+eps);
 end
 
 features(10) = -sum(concurrence_x .* log(concurrence_x+eps));
 features(11) = -sum(concurrence_y .* log(concurrence_y+eps));
 
-index_h = 1:height;
-index_w = 1:width;
-
-numerator = sum((index_h .* concurrence_x') .* (index_w .* concurrence_y));
- 
-denominator1 = sum(concurrence_x' .* (index_h - sum(index_h .* concurrence_x')));
-denominator2 = sum(concurrence_y .* (index_w - sum(index_w .* concurrence_y)));
+index = 1:slide;
+numerator = sum(index .* concurrence_x) * sum(index .* concurrence_y);
+denominator1 = sum(concurrence_x .* (index - sum(index .* concurrence_x)).^2);
+denominator2 = sum(concurrence_y .* (index - sum(index .* concurrence_y)).^2);
 
 features(12) = numerator / denominator1 / denominator2;
 
--- feature_extract/feature_yan.m
+++ feature_extract/feature_yan.m
@@ -5,13 +5,20 @@
 % matrix                QMDCT matrix
 % T                     threshold value
 % -----------------------------------------output
-% feature               feature vector
+% features              feature vector
 
-function feature = feature_yan(matrix, T)
+function features = feature_yan(matrix, T)
 
 concurrence_matrix_0 = get_concurrence(matrix, T, 0);
 concurrence_matrix_45 = get_concurrence(matrix, T, 45);
 concurrence_matrix_90 = get_concurrence(matrix, T, 90);
 concurrence_matrix_135 = get_concurrence(matrix, T, 135);
 
-feature = [feature1;feature2;feature3;feature4];
+features1 = get_concurrence_features(concurrence_matrix_0);
+features2 = get_concurrence_features(concurrence_matrix_45);
+features3 = get_concurrence_features(concurrence_matrix_90);
+features4 = get_concurrence_features(concurrence_matrix_135);
+
+features = [features1,features2,features3,features4];
+
+end
