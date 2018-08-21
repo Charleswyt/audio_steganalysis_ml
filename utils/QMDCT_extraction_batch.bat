b19a40fb19df098989222eff3f9d31c3825568bf
Charles_wyt <wangyuntao16@mails.ucas.ac.cn>
2018-08-22 00:02:56 +0800

    update

--- utils/QMDCT_extraction_batch.bat
+++ utils/QMDCT_extraction_batch.bat
@@ -1,8 +1,23 @@
-python3 .\QMDCT_extractor.py E:\Myself\2.database\3.cover\cover_10s\128
-python3 .\QMDCT_extractor.py E:\Myself\2.database\3.cover\cover_10s\192
-python3 .\QMDCT_extractor.py E:\Myself\2.database\3.cover\cover_10s\256
-python3 .\QMDCT_extractor.py E:\Myself\2.database\3.cover\cover_10s\320
-python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_128_W_2_H_7_ER_10
-python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_128_W_3_H_7_ER_10
-python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_128_W_4_H_7_ER_10
-python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_128_W_5_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_128_W_6_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_128_W_7_H_7_ER_10
+
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_192_W_2_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_192_W_3_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_192_W_4_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_192_W_5_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_192_W_6_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_192_W_7_H_7_ER_10
+
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_256_W_2_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_256_W_3_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_256_W_4_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_256_W_5_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_256_W_6_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_256_W_7_H_7_ER_10
+
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_320_W_2_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_320_W_3_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_320_W_4_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_320_W_5_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_320_W_6_H_7_ER_10
+python3 .\QMDCT_extractor.py E:\Myself\2.database\4.stego\EECS\EECS_B_320_W_7_H_7_ER_10
